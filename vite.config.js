b13ab783ab561971f9036951449533de36e6f2f0
khatera-ahad <khatera.ahad@gmail.com>
2026-08-19 23:40:12 -0300

    fix(gh-pages): correct Vite base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-to-gh-pages/",
-})
-
+  base: "/react-bank-managment-dashboard/",
+});
